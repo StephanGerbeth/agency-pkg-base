dbf0115d72a2c0e3222fff0314b0085d17691b50
Thorn Walli <thorn.walli@grabarzundpartner.intra>
2016-07-03 19:27:01 +0200

    added package

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # agency-pkg-base
+
+>Package-Pattern for [agency-boilerplate](https://github.com/StephanGerbeth/agency-boilerplate)
+
+Contains base components for agency-boilerplate and others agency-boilerplate compatible packages.
---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+"use strict";
+
+// require('./node_modules/agency-boilerplate/gulpfile');
+
+
+var options = require('minimist')(process.argv.slice(2));
+
+var gulp = require('gulp');
+var runSequence = require('run-sequence').use(gulp);
+require('agency-environment');
+require('agency-server');
+
+gulp.task('default', ['watch', 'server']);
+
+gulp.task('run', function(callback) {
+    if(!options.env || options.env === 'development') {
+        runSequence('prebuild', 'default', callback);
+    } else {
+        runSequence('build', 'server', callback);
+    }
+});
+
+process.once('SIGINT', function() { process.exit(0); });
