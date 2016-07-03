f79f2ce1f4decbdb431d76f03fd61c0ae8e30488
Thorn Walli <thorn.walli@grabarzundpartner.intra>
2016-07-03 19:33:13 +0200

    updated gulpfile, require from boilerplate

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,2 @@
 "use strict";
-
-// require('./node_modules/agency-boilerplate/gulpfile');
-
-
-var options = require('minimist')(process.argv.slice(2));
-
-var gulp = require('gulp');
-var runSequence = require('run-sequence').use(gulp);
-require('agency-environment');
-require('agency-server');
-
-gulp.task('default', ['watch', 'server']);
-
-gulp.task('run', function(callback) {
-    if(!options.env || options.env === 'development') {
-        runSequence('prebuild', 'default', callback);
-    } else {
-        runSequence('build', 'server', callback);
-    }
-});
-
-process.once('SIGINT', function() { process.exit(0); });
+require('agency-boilerplate/gulpfile');
